307bc60b397cedcbf1e1fc1be7fe83d9186410ab
rfahrn <rebecka.fahrni16@gmail.com>
2025-04-14 14:09:33 +0200

    update

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Simple Streamlit App")
+st.write("This is a simple Streamlit app that demonstrates the use of a slider and a button.")
+
+# enter a question for compendium.ch 
+question = st.text_input("Enter your question:")
+
