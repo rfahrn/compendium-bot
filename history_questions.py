6468b4c3792dd0e97f90f418bc31f35d0fc80527
rfahrn <rebecka.fahrni16@gmail.com>
2025-04-14 14:22:56 +0200

    update

--- history_questions.py
+++ history_questions.py
@@ -19,3 +19,46 @@ def get_history():
         # If the file does not exist, return an empty list
         return []
     
+    class HistoryQuestions:
+        def __init__(self):
+            self.history = []
+
+        def add_question(self, question):
+            """
+            Adds a question to the history list.
+            """
+            self.history.append(question)
+
+        def get_history(self):
+            """
+            Retrieves the history of questions asked.
+            """
+            return self.history
+        
+
+        def clear_history(self):
+            """
+            Clears the history of questions asked.
+            """
+            self.history = []
+            with open("history.txt", "w") as f:
+                f.write("")
+            
+        def save_history(self):
+            """
+            Saves the history to a file.
+            """
+            with open("history.txt", "w") as f:
+                for question in self.history:
+                    f.write(question + "\n")
+                    
+        def load_history(self):
+            """
+            Loads the history from a file.
+            """
+            try:
+                with open("history.txt", "r") as f:
+                    self.history = [line.strip() for line in f.readlines()]
+            except FileNotFoundError:
+                self.history = []
+    
